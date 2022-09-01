1abdd6231b1daf9e7218a7142a6f36abd6a973d9
alejandro taichi suarez <alejandrotaichisuarezuy@gmail.com>
2022-09-01 13:13:13 -0300

    CRUD created
    finally created CRUD. RPS finally is over in moves. 1/9/22 31:56

---
+++ Controllers/GameLogsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using APItaiRPS;
+using APItaiRPS.Data;
+
+namespace APItaiRPS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameLogsController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public GameLogsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/GameLogs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GameLog>>> GetGameLogs()
+        {
+          if (_context.GameLogs == null)
+          {
+              return NotFound();
+          }
+            return await _context.GameLogs.ToListAsync();
+        }
+
+        // GET: api/GameLogs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GameLog>> GetGameLog(int id)
+        {
+          if (_context.GameLogs == null)
+          {
+              return NotFound();
+          }
+            var gameLog = await _context.GameLogs.FindAsync(id);
+
+            if (gameLog == null)
+            {
+                return NotFound();
+            }
+
+            return gameLog;
+        }
+
+        // PUT: api/GameLogs/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGameLog(int id, GameLog gameLog)
+        {
+            if (id != gameLog.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(gameLog).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GameLogExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/GameLogs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<GameLog>> PostGameLog(GameLog gameLog)
+        {
+          if (_context.GameLogs == null)
+          {
+              return Problem("Entity set 'DataContext.GameLogs'  is null.");
+          }
+            _context.GameLogs.Add(gameLog);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGameLog", new { id = gameLog.Id }, gameLog);
+        }
+
+        // DELETE: api/GameLogs/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGameLog(int id)
+        {
+            if (_context.GameLogs == null)
+            {
+                return NotFound();
+            }
+            var gameLog = await _context.GameLogs.FindAsync(id);
+            if (gameLog == null)
+            {
+                return NotFound();
+            }
+
+            _context.GameLogs.Remove(gameLog);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GameLogExists(int id)
+        {
+            return (_context.GameLogs?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: myAllowSpecificOrigins,
         builder =>
         {
-            builder.WithOrigins("http://localhost:7000")
+            builder.WithOrigins("http://localhost:4200")
             .AllowAnyMethod()
             .AllowAnyHeader();
         });
@@ -47,3 +47,4 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
